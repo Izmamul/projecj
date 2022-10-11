b96769d1b17c4f1a8eb5645a2110f0d0ade9f83c
Izmamul <99896906+Izmamul@users.noreply.github.com>
2022-10-11 21:40:55 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -2,7 +2,7 @@
  navbar=document.querySelector('.navbar')
  navList=document.querySelector('.nav-list')
  rightnav=document.querySelector('.rightnav')
- burger.addEventListener('click',()=>{
+
 	rightnav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
  	navbar.classList.toggle('h-nav-resp');
